ecc072167fe540f10e3193d7559dd3b5e39a2655
saravatss <rebback89@gmail.com>
2024-02-03 11:29:52 +0300

    src/main.js

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-adfvdfv
+  <router-link to="/">
+    Главная
+  </router-link>
+
+  <router-link to="/catalog">
+    Каталог
+  </router-link>
+
+  <hr>
+
+  <router-view/>
 </template>
---
+++ src/pages/catalog/id.vue
@@ -0,0 +1,11 @@
+<template>
+    {{ route.params.id }}
+</template>
+
+<script setup>
+    import { useRoute } from "vue-router";
+
+    const route = useRoute();
+
+    console.log(route);
+</script>
